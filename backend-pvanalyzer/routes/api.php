5e5a71b152f72b918f8a640b2fe3f283f316756d
Mariusz Dąbrowski <mariuszd49@gmail.com>
2022-01-25 21:41:00 +0100

    feat: add endpoints  path to api route file

--- backend-pvanalyzer/routes/api.php
+++ backend-pvanalyzer/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PVInstallationController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,21 @@ use App\Http\Controllers\PVInstallationController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::resource('/pv-installation', PVInstallationController::class);
+
+//Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/pv-installation/{id}', [App\Http\Controllers\PVInstallationController::class, 'show']);
+
+
+//Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function()
+{
+    Route::post('/pv-installation', [App\Http\Controllers\PVInstallationController::class, 'store']);
+    Route::put('/pv-installation/{id}', [App\Http\Controllers\PVInstallationController::class, 'update']);
+    Route::delete('/pv-installation/{id}', [App\Http\Controllers\PVInstallationController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
