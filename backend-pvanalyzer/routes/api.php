52ca53e9533b43f1f43cee3834a4dc81d1ba32a0
Mariusz Dąbrowski <mariuszd49@gmail.com>
2022-02-01 23:17:14 +0100

    feat: route multi models binding

--- backend-pvanalyzer/routes/api.php
+++ backend-pvanalyzer/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PVInstallationController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PVInstallationCounterReadingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,9 @@ use App\Http\Controllers\AuthController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// Route::model('pv-installations', App\Models\PVInstallation::class);
+// Route::model('counterReading', App\Models\CounterReading::class);
+
 
 //Public routes
 Route::post('/register', [AuthController::class, 'register']);
@@ -23,13 +27,18 @@ Route::post('/login', [AuthController::class, 'login']);
 
 
 //Protected routes
-Route::group(['middleware' => ['auth:sanctum']], function()
-{
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    //PVInstallations
     Route::get('/pv-installations', [App\Http\Controllers\PVInstallationController::class, 'index']);
     Route::post('/pv-installations', [App\Http\Controllers\PVInstallationController::class, 'store']);
     Route::get('/pv-installations/{pVInstallation}', [App\Http\Controllers\PVInstallationController::class, 'show']);
     Route::put('/pv-installations/{pVInstallation}', [App\Http\Controllers\PVInstallationController::class, 'update']);
     Route::delete('/pv-installations/{pVInstallation}', [App\Http\Controllers\PVInstallationController::class, 'destroy']);
+    //Counter Readings
+    Route::apiResource('pv-installations.counter-readings', PVInstallationCounterReadingController::class)->parameters([
+        'pv-installations' => 'pVInstallation',
+        'counter-readings' => 'counterReading'
+    ]);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
