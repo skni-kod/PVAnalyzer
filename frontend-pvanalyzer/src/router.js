75604f68369ad4e7d41199cfba4f2d04dc9d58a0
Mariusz Dąbrowski <mariuszd49@gmail.com>
2022-02-19 15:34:30 +0100

    refactor: some cleanup in code

--- frontend-pvanalyzer/src/router.js
+++ frontend-pvanalyzer/src/router.js
@@ -2,27 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import LoginPage from './pages/auth/LoginPage.vue';
 import RegisterPage from './pages/auth/RegisterPage.vue';
-// import Dashboard from './pages/dashboard/Dashboard.vue'; 
+import Dashboard from './pages/dashboard/Dashboard.vue'; 
 import PVInstallation from './pages/pv-installation/PVInstallation.vue'
 import ProfilePage from './pages/auth/ProfilePage.vue';
 import store from './store/index.js';
-
-// import ChangePassword from './components/auth/ChangePassword.vue'
-// import Chart from './components/charts/ChartContainer.vue';
-import Chart from './components/charts/LineChartCard.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [ 
         { path: '/', redirect: '/dashboard' },
-        // { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true }},
+        { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true }},
         { path: '/login', component: LoginPage, props: true},
         { path: '/register', component: RegisterPage},
-        // { path: '/pv-installation', component: ProfilePage, meta: { requiresAuth: true }},     //info o instalacji
         { path: '/pv-installation', component: PVInstallation, meta: { requiresAuth: true }},     //info o instalacji
-        { path: '/dashboard', component: Chart, meta: { requiresAuth: true }},
         { path: '/readings', component: null,  meta: { requiresAuth: true }},                      //odczyty
-        // { path: '/profile', component: ChangePassword,},
         { path: '/profile', component: ProfilePage, meta: { requiresAuth: true }},
     ]
 });
