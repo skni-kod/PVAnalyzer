6ea259e980c58585720ef5e47b8ba3bd22b2a7db
Mariusz Dąbrowski <mariuszd49@gmail.com>
2022-03-06 13:22:23 +0100

    feat: validation error message component

---
+++ frontend-pvanalyzer/src/components/validation/ErrorMessage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="error-message" v-if="value.$error">
+          <span v-for="error in value.$errors" :key="error.$uid">{{
+            error.$message
+          }}</span>
+        </div>
+</template>
+
+<script>
+export default {
+    props:{
+    value: {
+      type: Object,
+      required: true,
+    }
+    }
+
+}
+</script>
+
+<style scoped>
+.error-message {
+  display: flex;
+  flex-direction: column;
+  color: red;
+  font-size: 12px;
+}
+</style>
